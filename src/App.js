2cdd645c3a1e8729fc8a645a04f8819f21055b0e
sif <sifddin@hotmail.cmo>
2022-08-07 16:29:09 +0200

    add fire class

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-     <Calculator/>
+     <Calculator className="fire"/>
       </header>
     </div>
   );
